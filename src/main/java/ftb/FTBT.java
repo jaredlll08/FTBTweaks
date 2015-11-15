c5df35ab72e339045fbd09b9026d31b6f7f8ce55
Dries007 <admin@dries007.net>
2015-11-15 01:15:26 +0100

    fixed #1 Also fixed dependency information not being used.

--- src/main/java/ftb/FTBT.java
+++ src/main/java/ftb/FTBT.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "FTBT", name = "FTB Tweaks", version = "1.0.1", dependencies = "required-after:MineTweaker3;required-after:FTBL")
+@Mod(modid = "FTBT", name = "FTB Tweaks", version = "1.0.1", dependencies = "required-after:MineTweaker3;required-after:FTBL", useMetadata = false)
 public class FTBT {
 
 	@EventHandler
--- src/main/java/ftb/ReloadHandler.java
+++ src/main/java/ftb/ReloadHandler.java
@@ -2,10 +2,16 @@ package ftb;
 
 import minetweaker.MineTweakerAPI;
 import minetweaker.MineTweakerImplementationAPI;
+import minetweaker.mc1710.util.MineTweakerHacks;
+import minetweaker.runtime.IScriptProvider;
 import minetweaker.runtime.providers.ScriptProviderCascade;
 import minetweaker.runtime.providers.ScriptProviderDirectory;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import ftb.lib.api.EventFTBModeSet;
+import net.minecraft.server.MinecraftServer;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class ReloadHandler
 {
@@ -20,7 +26,19 @@ public class ReloadHandler
 			e.getCommonFile("scripts").mkdirs();
 		}
 		MineTweakerAPI.tweaker.rollback();
-		MineTweakerImplementationAPI.setScriptProvider(new ScriptProviderCascade(new ScriptProviderDirectory(e.getCommonFile("scripts")), new ScriptProviderDirectory(e.getFile("scripts"))));
+
+		ArrayList<IScriptProvider> providers = new ArrayList<IScriptProvider>();
+
+		providers.add(new ScriptProviderDirectory(e.getCommonFile("scripts")));
+		providers.add(new ScriptProviderDirectory(e.getFile("scripts")));
+
+		// Don't have to mkdir these because MT does that for us already.
+		providers.add(new ScriptProviderDirectory(new File("scripts")));
+		if (e.side.isServer()) {
+			providers.add(new ScriptProviderDirectory(new File(MineTweakerHacks.getWorldDirectory(MinecraftServer.getServer()), "scripts")));
+		}
+
+		MineTweakerImplementationAPI.setScriptProvider(new ScriptProviderCascade(providers.toArray(new IScriptProvider[providers.size()])));
 		MineTweakerImplementationAPI.reload();
 	}
 }
