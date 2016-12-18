d2839c241cc0019d5f1902e21bfdc53279e36caf
Jared <jaredlll08@gmail.com>
2016-12-18 17:10:33 +0200

    Update to 1.10.2

---
+++ src/main/java/com/feed_the_beast/ftbt/FTBT.java
@@ -0,0 +1,9 @@
+package com.feed_the_beast.ftbt;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "ftbtweaks", name = "FTB Tweaks", version = "2.0.0", dependencies = "required-after:MineTweaker3;required-after:ftbl", useMetadata = false)
+public class FTBT {
+	
+	
+}
