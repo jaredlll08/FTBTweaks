02db8bcf02b96a801d15c61b96828b2bdb1b1387
phit <me@phit.link>
2017-04-05 06:25:46 +0200

    fix overriding sync when playing on a dedicated server

--- src/main/java/com/feed_the_beast/ftbt/FTBTweaks.java
+++ src/main/java/com/feed_the_beast/ftbt/FTBTweaks.java
@@ -13,12 +13,12 @@ import net.minecraftforge.fml.relauncher.Side;
 import java.io.File;
 import java.util.ArrayList;
 
+import static com.feed_the_beast.ftbt.FTBT.proxy;
+
 public enum FTBTweaks implements IFTBLibPlugin {
-	
 	@FTBLibPlugin
 	INSTANCE;
-	
-	
+
 	public static FTBLibAPI API;
 	
 	@Override
@@ -29,29 +29,32 @@ public enum FTBTweaks implements IFTBLibPlugin {
 	
 	@Override
 	public void onReload(Side side, ICommandSender sender, EnumReloadType type) {
-		File scripts = new File((side.isServer() ? API.getServerData() : API.getClientData()).getPackMode().getFolder(), "scripts");
-//		File scripts = new File(API.getServerData().getPackMode().getFolder(), "scripts");
-//		File scriptsCommon = new File(API.getPackModes().getDefault().getFolder(), "scripts");
-		
-		if(!scripts.exists()){
-			scripts.mkdirs();
+		if(side.isServer() || side.isClient() && proxy.isSinglePlayer()){
+			File scripts = new File((side.isServer() ? API.getServerData() : API.getClientData()).getPackMode().getFolder(), "scripts");
+//		    File scripts = new File(API.getServerData().getPackMode().getFolder(), "scripts");
+//		    File scriptsCommon = new File(API.getPackModes().getDefault().getFolder(), "scripts");
+
+			if(!scripts.exists()){
+				scripts.mkdirs();
+			}
+
+			MineTweakerAPI.tweaker.rollback();
+			ArrayList<IScriptProvider> providers = new ArrayList<IScriptProvider>();
+
+			providers.add(new ScriptProviderDirectory(scripts));
+
+			// Don't have to mkdir these because MT does that for us already.
+			providers.add(new ScriptProviderDirectory(new File("scripts")));
+			if (side.isServer()) {
+				providers.add(new ScriptProviderDirectory(new File(MineTweakerHacks.getWorldDirectory(sender.getServer()), "scripts")));
+			}
+
+			MineTweakerImplementationAPI.setScriptProvider(new ScriptProviderCascade(providers.toArray(new IScriptProvider[providers.size()])));
+			MineTweakerImplementationAPI.reload();
+
+			if (side.isServer()) {
+				MineTweakerMod.NETWORK.sendToAll(new MineTweakerLoadScriptsPacket(MineTweakerAPI.tweaker.getScriptData()));
+			}
 		}
-		
-		
-		MineTweakerAPI.tweaker.rollback();
-		ArrayList<IScriptProvider> providers = new ArrayList<IScriptProvider>();
-		
-		providers.add(new ScriptProviderDirectory(scripts));
-		
-		// Don't have to mkdir these because MT does that for us already.
-		providers.add(new ScriptProviderDirectory(new File("scripts")));
-		if (side==Side.SERVER) {
-			providers.add(new ScriptProviderDirectory(new File(MineTweakerHacks.getWorldDirectory(sender.getServer()), "scripts")));
-		}
-		
-		MineTweakerImplementationAPI.setScriptProvider(new ScriptProviderCascade(providers.toArray(new IScriptProvider[providers.size()])));
-		MineTweakerImplementationAPI.reload();
-		MineTweakerMod.NETWORK.sendToAll(new MineTweakerLoadScriptsPacket(MineTweakerAPI.tweaker.getScriptData()));
-		
 	}
 }
