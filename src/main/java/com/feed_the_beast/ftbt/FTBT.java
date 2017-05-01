8bb7ed7df673652c6d9c03dd6aa5fefa980b3d40
LatvianModder <latvianmodder@gmail.com>
2017-05-01 11:01:10 +0300

    Removed MT dependency, since modID is different in 1.11 from 1.10

--- src/main/java/com/feed_the_beast/ftbt/FTBT.java
+++ src/main/java/com/feed_the_beast/ftbt/FTBT.java
@@ -3,7 +3,7 @@ package com.feed_the_beast.ftbt;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 
-@Mod(modid = "ftbtweaks", name = "FTB Tweaks", version = "2.0.1", dependencies = "required-after:MineTweaker3;required-after:ftbl", useMetadata = false, acceptedMinecraftVersions = "[1.10,1.12)")
+@Mod(modid = "ftbtweaks", name = "FTB Tweaks", version = "2.0.1", dependencies = "required-after:ftbl", useMetadata = false, acceptedMinecraftVersions = "[1.10,1.12)")
 public class FTBT
 {
     @SidedProxy(clientSide = "com.feed_the_beast.ftbt.ClientProxy", serverSide = "com.feed_the_beast.ftbt.ServerProxy")
