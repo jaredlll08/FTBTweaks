98bd872ad0daaeb7cc1df9b74819b2343688b8f5
LatvianModder <latvianmodder@gmail.com>
2017-05-01 10:39:58 +0300

    Updated FTBLib, added build.gradle dependency, ported to 1.11

--- src/main/java/com/feed_the_beast/ftbt/FTBT.java
+++ src/main/java/com/feed_the_beast/ftbt/FTBT.java
@@ -3,8 +3,9 @@ package com.feed_the_beast.ftbt;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 
-@Mod(modid = "ftbtweaks", name = "FTB Tweaks", version = "2.0.1", dependencies = "required-after:MineTweaker3;required-after:ftbl", useMetadata = false)
-public class FTBT {
-    @SidedProxy(clientSide="com.feed_the_beast.ftbt.ClientProxy", serverSide="com.feed_the_beast.ftbt.ServerProxy")
+@Mod(modid = "ftbtweaks", name = "FTB Tweaks", version = "2.0.1", dependencies = "required-after:MineTweaker3;required-after:ftbl", useMetadata = false, acceptedMinecraftVersions = "[1.10,1.12)")
+public class FTBT
+{
+    @SidedProxy(clientSide = "com.feed_the_beast.ftbt.ClientProxy", serverSide = "com.feed_the_beast.ftbt.ServerProxy")
     public static CommonProxy proxy;
 }
